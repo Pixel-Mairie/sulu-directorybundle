4e230b1df955d5073f428bd07921dd3fff70d147
Matthieu BARBE <matthieu@pixel-developpement.com>
2026-05-04 11:18:15 +0200

    + Migration annotations to attributes for entities

--- src/Entity/Setting.php
+++ src/Entity/Setting.php
@@ -8,11 +8,9 @@ use Sulu\Bundle\MediaBundle\Entity\MediaInterface;
 use Sulu\Component\Persistence\Model\AuditableInterface;
 use Sulu\Component\Persistence\Model\AuditableTrait;
 
-/**
- * @ORM\Entity()
- * @ORM\Table(name="directory_setting")
- * @Serializer\ExclusionPolicy("all")
- */
+#[ORM\Entity()]
+#[ORM\Table(name: "directory_setting")]
+#[Serializer\ExclusionPolicy("all")]
 class Setting implements AuditableInterface
 {
     use AuditableTrait;
@@ -23,26 +21,22 @@ class Setting implements AuditableInterface
 
     public const SECURITY_CONTEXT = "directory_settings.settings";
 
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     * @Serializer\Expose()
-     */
+    #[ORM\Id()]
+    #[ORM\GeneratedValue()]
+    #[ORM\Column(type: "integer")]
+    #[Serializer\Expose()]
     private ?int $id = null;
 
     /**
-     * @ORM\Column(type="json", nullable=true)
-     * @Serializer\Expose()
      * @var array<mixed>|null
      */
+    #[ORM\Column(type: "json", nullable: true)]
+    #[Serializer\Expose()]
     private ?array $location = null;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=MediaInterface::class)
-     * @ORM\JoinColumn(onDelete="SET NULL")
-     * @Serializer\Expose()
-     */
+    #[ORM\ManyToOne(targetEntity: MediaInterface::class)]
+    #[ORM\JoinColumn(onDelete: "SET NULL")]
+    #[Serializer\Expose()]
     private ?MediaInterface $defaultImage = null;
 
     public function getId(): ?int
